a55ae51116fc9dd6a24fe77ac37e7d19a1dd6bc9
jessieYay <99616303+jessieYay@users.noreply.github.com>
2022-09-07 20:26:40 +0200

    🔴 Should return true test

--- src/test/java/groupId/PokerHandTest.java
+++ src/test/java/groupId/PokerHandTest.java
@@ -2,11 +2,23 @@ package groupId;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PokerHandTest {
 
     @Test
 
     void testHand(){
 
+    }
+
+    @Test
+
+    void testTru(){
+        assertTrue(checkThis());
+
+    }
+    public boolean checkThis(){
+        return false;
     }
 }
